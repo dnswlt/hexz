82adcd13663c6b5b3554779b5f4ebcb734ca658e
Dennis Walter <dennis.walter@gmail.com>
2023-03-25 18:01:00 +0100

    Add missing server binary.

---
+++ cmd/server/hexz.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/dnswlt/hackz/hexz"
+)
+
+var ()
+
+func main() {
+	cfg := &hexz.ServerConfig{1}
+
+	flag.StringVar(&cfg.ServerAddress, "address", "", "Address on which to listen")
+	flag.IntVar(&cfg.ServerPort, "port", 8084, "Port on which to listen")
+	flag.StringVar(&cfg.DocumentRoot, "document-root", ".", "Root directory from which to serve files")
+	flag.IntVar(&cfg.GameGcDelaySeconds, "gcdelay", 5,
+		"Seconds to wait before deleting a disconnected player from a game")
+	flag.StringVar(&cfg.TlsCertChain, "tls-cert", "", "Path to chain.pem for TLS")
+	flag.StringVar(&cfg.TlsPrivKey, "tls-key", "", "Path to privkey.pem for TLS")
+	flag.Parse()
+
+	hexz.Serve(cfg)
+}
