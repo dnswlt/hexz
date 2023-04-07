1cc03eb31af93b8fb3d879208811117388706ba2
Dennis Walter <dennis.walter@gmail.com>
2023-04-07 10:06:53 +0200

    Refactoring game engine to its own file.

---
+++ api.go
@@ -0,0 +1,58 @@
+package hexz
+
+type GameState string
+
+const (
+	Initial  GameState = "initial"
+	Running  GameState = "running"
+	Finished GameState = "finished"
+)
+
+// JSON for server responses.
+
+type ServerEvent struct {
+	Timestamp     string   `json:"timestamp"`
+	Board         *Board   `json:"board"`
+	Role          int      `json:"role"` // 0: spectator, 1, 2: players
+	Announcements []string `json:"announcements"`
+	DebugMessage  string   `json:"debugMessage"`
+	ActiveGames   []string `json:"activeGames"`
+	LastEvent     bool     `json:"lastEvent"` // Signals to clients that this is the last event they will receive.
+}
+
+type Board struct {
+	Turn         int            `json:"turn"`
+	Move         int            `json:"move"`
+	LastRevealed int            `json:"-"` // Move at which fields were last revealed
+	Fields       [][]Field      `json:"fields"`
+	Score        []int          `json:"score"`
+	Resources    []ResourceInfo `json:"resources"`
+	State        GameState      `json:"state"`
+}
+
+type Field struct {
+	Type     CellType `json:"type"`
+	Owner    int      `json:"owner"` // Player number owning this field. 0 for unowned fields.
+	Hidden   bool     `json:"hidden"`
+	Lifetime int      `json:"-"` // Moves left until this cell gets cleared. -1 means infinity.
+}
+
+// Information about the resources each player has left.
+type ResourceInfo struct {
+	NumPieces map[CellType]int `json:"numPieces"`
+}
+
+type CellType int
+
+const (
+	cellNormal CellType = iota
+	cellDead
+	cellFire
+	cellFlag
+	cellPest
+	cellDeath
+)
+
+func (c CellType) valid() bool {
+	return c >= cellNormal && c <= cellDeath
+}
--- server.go
+++ server.go
@@ -55,9 +55,6 @@ func NewServer(cfg *ServerConfig) *Server {
 }
 
 const (
-	numFieldsFirstRow = 10
-	numBoardRows      = 11
-
 	maxLoggedInPlayers = 10000
 
 	playerIdCookieName   = "playerId"
@@ -94,198 +91,12 @@ func isFavicon(path string) bool {
 	return false
 }
 
-type GameDesc struct {
-	gameType      GameType
-	numPlayers    int
-	initialPieces map[CellType]int
-}
-
-var (
-	allGameTypes = map[GameType]GameDesc{
-		gameTypeClassic: {
-			gameType:   gameTypeClassic,
-			numPlayers: 2,
-			initialPieces: map[CellType]int{
-				cellNormal: -1, // unlimited
-				cellFire:   1,
-				cellFlag:   1,
-				cellPest:   1,
-				cellDeath:  1,
-			},
-		},
-		gameTypeFreeform: {
-			gameType:   gameTypeFreeform,
-			numPlayers: 2,
-			initialPieces: map[CellType]int{
-				cellNormal: -1, // unlimited
-				cellFire:   -1,
-				cellFlag:   -1,
-				cellPest:   -1,
-				cellDeath:  -1,
-			},
-		},
-	}
-)
-
-type GameType string
-
-const (
-	gameTypeClassic  GameType = "classic"
-	gameTypeFreeform GameType = "freeform"
-)
-
 type GameHandle struct {
 	Id           string
 	Started      time.Time
 	gameType     GameType
 	controlEvent chan ControlEvent // The channel to communicate with the game coordinating goroutine.
 	done         chan struct{}     // Closed by the game master goroutine when it is done.
-}
-
-type GameState string
-
-const (
-	Initial  GameState = "initial"
-	Running  GameState = "running"
-	Finished GameState = "finished"
-	Aborted  GameState = "aborted"
-)
-
-// JSON for server responses.
-
-type ServerEvent struct {
-	Timestamp     string   `json:"timestamp"`
-	Board         *Board   `json:"board"`
-	Role          int      `json:"role"` // 0: spectator, 1, 2: players
-	Announcements []string `json:"announcements"`
-	DebugMessage  string   `json:"debugMessage"`
-	ActiveGames   []string `json:"activeGames"`
-	LastEvent     bool     `json:"lastEvent"` // Signals to clients that this is the last event they will receive.
-}
-
-type Board struct {
-	Turn         int            `json:"turn"`
-	Move         int            `json:"move"`
-	LastRevealed int            `json:"-"` // Move at which fields were last revealed
-	Fields       [][]Field      `json:"fields"`
-	Score        []int          `json:"score"`
-	Resources    []ResourceInfo `json:"resources"`
-	State        GameState      `json:"state"`
-}
-
-type Field struct {
-	Type     CellType `json:"type"`
-	Owner    int      `json:"owner"` // Player number owning this field. 0 for unowned fields.
-	Hidden   bool     `json:"hidden"`
-	Lifetime int      `json:"-"` // Moves left until this cell gets cleared. -1 means infinity.
-}
-
-type CellType int
-
-const (
-	cellNormal CellType = iota
-	cellDead
-	cellFire
-	cellFlag
-	cellPest
-	cellDeath
-)
-
-func (c CellType) valid() bool {
-	return c >= cellNormal && c <= cellDeath
-}
-
-type GameEngine interface {
-	Init(numPlayers int)
-	Reset()
-	MakeMove(playerNum int, e ControlEventMove) bool
-	Board() *Board
-	IsDone() bool
-	Winner() (playerNum int) // Results are only meaningful if IsDone() is true. 0 for draw.
-}
-
-type GameEngineClassic struct {
-	board *Board
-}
-
-func (g *GameEngineClassic) Board() *Board { return g.board }
-func (g *GameEngineClassic) Init(numPlayers int) {
-	if numPlayers != 2 {
-		panic("Cannot play a classic game with " + fmt.Sprint(numPlayers) + " players")
-	}
-	g.board = NewBoard()
-	InitBoard(g.board, numPlayers, gameTypeClassic)
-}
-
-func (g *GameEngineClassic) Reset() {
-	g.Init(2)
-}
-
-func (g *GameEngineClassic) IsDone() bool { return g.board.State == Finished }
-
-func (g *GameEngineClassic) Winner() (playerNum int) {
-	if !g.IsDone() {
-		return 0
-	}
-	maxIdx := -1
-	maxScore := -1
-	uniq := false
-	for i, s := range g.board.Score {
-		if s > maxScore {
-			maxScore = s
-			maxIdx = i
-			uniq = true
-		} else if s == maxScore {
-			uniq = false
-		}
-	}
-	if uniq {
-		return maxIdx
-	}
-	return 0
-}
-
-type GameEngineFreeform struct {
-	board *Board
-}
-
-func (g *GameEngineFreeform) Board() *Board { return g.board }
-
-func (g *GameEngineFreeform) Init(numPlayers int) {
-	g.board = NewBoard()
-	InitBoard(g.board, numPlayers, gameTypeFreeform)
-}
-
-func (g *GameEngineFreeform) Reset()       { g.board = NewBoard() }
-func (g *GameEngineFreeform) IsDone() bool { return false }
-func (g *GameEngineFreeform) Winner() (playerNum int) {
-	return 0 // No one ever wins here.
-}
-
-// Information about the resources each player has left.
-type ResourceInfo struct {
-	NumPieces map[CellType]int `json:"numPieces"`
-}
-
-func InitBoard(board *Board, numPlayers int, gameType GameType) {
-	board.Score = make([]int, numPlayers)
-
-	gd, ok := allGameTypes[gameType]
-	if !ok {
-		panic("Game type " + gameType + " not in allGameTypes")
-	}
-	board.Resources = make([]ResourceInfo, numPlayers)
-	for i := 0; i < numPlayers; i++ {
-		ri := ResourceInfo{
-			NumPieces: make(map[CellType]int),
-		}
-		for ct, n := range gd.initialPieces {
-			ri.NumPieces[ct] = n
-		}
-		board.Resources[i] = ri
-	}
-
-	board.State = Running
 }
 
 // JSON for incoming requests from UI clients.
@@ -356,9 +167,7 @@ type ControlEventUnregister struct {
 
 type ControlEventMove struct {
 	PlayerId string
-	Row      int
-	Col      int
-	Type     CellType
+	MoveRequest
 }
 
 type ControlEventReset struct {
@@ -423,35 +232,6 @@ func NewGame(id string, gameType GameType) *GameHandle {
 	}
 }
 
-func NewGameEngine(gameType GameType) GameEngine {
-	switch gameType {
-	case gameTypeClassic:
-		return &GameEngineClassic{}
-	case gameTypeFreeform:
-		return &GameEngineFreeform{}
-	default:
-		panic("Unconsidered game type: " + gameType)
-	}
-}
-
-// Creates a new, empty board. Callers need to set the Resources of the board later!
-func NewBoard() *Board {
-	const numFields = numFieldsFirstRow*((numBoardRows+1)/2) + (numFieldsFirstRow-1)*(numBoardRows/2)
-	arr := make([]Field, numFields)
-	fields := make([][]Field, numBoardRows)
-	start := 0
-	for i := 0; i < len(fields); i++ {
-		end := start + numFieldsFirstRow - i%2
-		fields[i] = arr[start:end]
-		start = end
-	}
-	return &Board{
-		Turn:   1, // Player 1 begins
-		Fields: fields,
-		State:  Initial,
-	}
-}
-
 // Looks up the game ID from the URL path.
 func gameIdFromPath(path string) string {
 	pathSegs := strings.Split(path, "/")
@@ -460,324 +240,6 @@ func gameIdFromPath(path string) string {
 		return pathSegs[l-1]
 	}
 	return ""
-}
-
-func (f *Field) occupied() bool {
-	return f.Type == cellDead || f.Owner > 0
-}
-
-func recomputeScoreAndState(b *Board) {
-	s := []int{0, 0}
-	openCells := 0
-	for _, row := range b.Fields {
-		for _, fld := range row {
-			if fld.Owner > 0 && !fld.Hidden {
-				s[fld.Owner-1]++
-			}
-			if !fld.occupied() || fld.Hidden {
-				// Don't finish the game until all cells are owned and not hidden, or dead.
-				openCells++
-			}
-		}
-	}
-	b.Score = s
-	if openCells == 0 {
-		// No more inconclusive cells: game is finished
-		b.State = Finished
-	}
-}
-
-type idx struct {
-	r, c int
-}
-
-func (b *Board) valid(x idx) bool {
-	return x.r >= 0 && x.r < len(b.Fields) && x.c >= 0 && x.c < len(b.Fields[x.r])
-}
-
-// Populates ns with valid indices of all neighbor cells. Returns the number of neighbor cells.
-// ns must have enough capacity to hold all neighbors. You should pass in a [6]idx slice.
-func (b *Board) neighbors(x idx, ns []idx) int {
-	shift := x.r & 1 // Depending on the row, neighbors below and above are shifted.
-	k := 0
-	ns[k] = idx{x.r, x.c + 1}
-	if b.valid(ns[k]) {
-		k++
-	}
-	ns[k] = idx{x.r - 1, x.c + shift}
-	if b.valid(ns[k]) {
-		k++
-	}
-	ns[k] = idx{x.r - 1, x.c - 1 + shift}
-	if b.valid(ns[k]) {
-		k++
-	}
-	ns[k] = idx{x.r, x.c - 1}
-	if b.valid(ns[k]) {
-		k++
-	}
-	ns[k] = idx{x.r + 1, x.c - 1 + shift}
-	if b.valid(ns[k]) {
-		k++
-	}
-	ns[k] = idx{x.r + 1, x.c + shift}
-	if b.valid(ns[k]) {
-		k++
-	}
-	return k
-}
-
-func floodFill(b *Board, x idx, cb func(idx) bool) {
-	var ns [6]idx
-	if !cb(x) {
-		return
-	}
-	n := b.neighbors(x, ns[:])
-	for i := 0; i < n; i++ {
-		floodFill(b, ns[i], cb)
-	}
-}
-
-func (c CellType) lifetime() int {
-	switch c {
-	case cellFire, cellDead, cellDeath:
-		return 1
-	case cellPest:
-		return 3
-	}
-	return -1 // Live forever
-}
-
-func occupyFields(b *Board, playerNum, r, c int, ct CellType) int {
-	// Create a board-shaped 2d array that indicates which neighboring cell of (i, j)
-	// it shares the free area with.
-	// Then find the smallest of these areas and occupy every free cell in it.
-	ms := make([][]int, len(b.Fields))
-	for k := 0; k < len(ms); k++ {
-		ms[k] = make([]int, len(b.Fields[k]))
-		for m := 0; m < len(ms[k]); m++ {
-			ms[k][m] = -1
-		}
-	}
-	f := &b.Fields[r][c]
-	f.Owner = playerNum
-	f.Type = ct
-	f.Hidden = true
-	f.Lifetime = ct.lifetime()
-	var areas [6]struct {
-		size      int    // Number of free cells in the area
-		flags     [2]int // Number of flags along the boundary
-		deadCells int    // Number of dead cells along the boundary
-	}
-	// If the current move sets a flag, this flag counts in all directions.
-	if ct == cellFlag {
-		for i := 0; i < len(areas); i++ {
-			areas[i].flags[playerNum-1]++
-		}
-	}
-	// Flood fill starting from each of (r, c)'s neighbors.
-	var ns [6]idx
-	n := b.neighbors(idx{r, c}, ns[:])
-	for k := 0; k < n; k++ {
-		if ms[ns[k].r][ns[k].c] != -1 {
-			// k's area is the same area as k-c for some c.
-			continue
-		}
-		floodFill(b, ns[k], func(x idx) bool {
-			if ms[x.r][x.c] == k {
-				// Already seen in this iteration.
-				return false
-			}
-			f := &b.Fields[x.r][x.c]
-			if f.occupied() {
-				// Occupied fields act as boundaries.
-				if f.Type == cellFlag {
-					areas[k].flags[f.Owner-1]++
-				} else if f.Type == cellDead {
-					areas[k].deadCells++
-				}
-				// Mark as seen to avoid revisiting boundaries.
-				ms[x.r][x.c] = k
-				return false
-			}
-			// Mark as seen and update area size.
-			ms[x.r][x.c] = k
-			areas[k].size++
-			return true
-		})
-	}
-	// If there is more than one area, we know we introduced a split, since the areas
-	// would have been connected by the previously free cell (r, c).
-	numOccupiedFields := 1
-	numAreas := 0
-	minK := -1
-	// Count the number of separated areas and find the smallest one.
-	for k := 0; k < n; k++ {
-		if areas[k].size > 0 && areas[k].deadCells == 0 {
-			numAreas++
-			if minK == -1 || areas[minK].size > areas[k].size {
-				minK = k
-			}
-		}
-	}
-	if numAreas > 1 {
-		// Now assign fields to player with most flags, or to current player on a tie.
-		occupator := playerNum
-		if areas[minK].flags[2-playerNum] > areas[minK].flags[playerNum-1] {
-			occupator = 3 - playerNum // The other player has more flags
-		}
-		for r := 0; r < len(b.Fields); r++ {
-			for c := 0; c < len(b.Fields[r]); c++ {
-				f := &b.Fields[r][c]
-				if ms[r][c] == minK && !f.occupied() {
-					numOccupiedFields++
-					f.Owner = occupator
-					f.Lifetime = cellNormal.lifetime()
-				}
-			}
-		}
-	}
-	return numOccupiedFields
-}
-
-func applyFireEffect(b *Board, r, c int) {
-	var ns [6]idx
-	n := b.neighbors(idx{r, c}, ns[:])
-	for i := 0; i < n; i++ {
-		f := &b.Fields[ns[i].r][ns[i].c]
-		f.Owner = 0
-		f.Type = cellDead
-		f.Hidden = false
-		f.Lifetime = cellDead.lifetime()
-	}
-}
-
-func applyPestEffect(b *Board) {
-	var ns [6]idx
-	for r := 0; r < len(b.Fields); r++ {
-		for c := 0; c < len(b.Fields[r]); c++ {
-			// A pest cell does not propagate in its first round.
-			if b.Fields[r][c].Type == cellPest && b.Fields[r][c].Lifetime < cellPest.lifetime() {
-				n := b.neighbors(idx{r, c}, ns[:])
-				for i := 0; i < n; i++ {
-					f := &b.Fields[ns[i].r][ns[i].c]
-					if f.Owner > 0 && f.Owner != b.Fields[r][c].Owner && f.Type == cellNormal {
-						// Pest only affects the opponent's normal cells.
-						f.Owner = b.Fields[r][c].Owner
-						f.Type = cellPest
-						f.Lifetime = cellPest.lifetime()
-					}
-				}
-			}
-		}
-	}
-}
-
-func (g *GameEngineFreeform) MakeMove(playerNum int, e ControlEventMove) bool {
-	board := g.board
-	if !board.valid(idx{e.Row, e.Col}) {
-		// Invalid move request.
-		return false
-	}
-	board.Move++
-	f := &board.Fields[e.Row][e.Col]
-	f.Owner = board.Turn
-	f.Type = e.Type
-	board.Turn++
-	if board.Turn > 2 {
-		board.Turn = 1
-	}
-	return true
-}
-
-func (g *GameEngineClassic) MakeMove(playerNum int, e ControlEventMove) bool {
-	board := g.board
-	turn := board.Turn
-	if playerNum != turn {
-		// Only allow moves by players whose turn it is.
-		return false
-	}
-	if !board.valid(idx{e.Row, e.Col}) || e.Type == cellDead {
-		// Invalid move request.
-		return false
-	}
-	if e.Type != cellNormal && board.Resources[turn-1].NumPieces[e.Type] == 0 {
-		// No pieces left of requested type
-		return false
-	}
-	numOccupiedFields := 0
-	revealBoard := e.Type != cellNormal && e.Type != cellFlag
-	if board.Fields[e.Row][e.Col].occupied() {
-		if board.Fields[e.Row][e.Col].Hidden && board.Fields[e.Row][e.Col].Owner == (3-turn) {
-			// Conflicting hidden moves. Leads to dead cell.
-			board.Move++
-			f := &board.Fields[e.Row][e.Col]
-			f.Owner = 0
-			f.Type = cellDead
-			f.Lifetime = cellDead.lifetime()
-			revealBoard = true
-		} else if e.Type == cellDeath {
-			// Death cell can be placed anywhere and will "kill" whatever was there before.
-			f := &board.Fields[e.Row][e.Col]
-			f.Owner = turn
-			f.Type = cellDeath
-			f.Hidden = false
-			f.Lifetime = cellDeath.lifetime()
-		} else {
-			// Cannot make move on already occupied field.
-			return false
-		}
-	} else {
-		// Free cell: occupy it.
-		board.Move++
-		f := &board.Fields[e.Row][e.Col]
-		if e.Type == cellFire {
-			// Fire cells take effect immediately.
-			f.Owner = turn
-			f.Type = e.Type
-			f.Lifetime = cellFire.lifetime()
-			applyFireEffect(board, e.Row, e.Col)
-		} else {
-			numOccupiedFields = occupyFields(board, turn, e.Row, e.Col, e.Type)
-		}
-	}
-	if e.Type != cellNormal {
-		board.Resources[turn-1].NumPieces[e.Type]--
-	}
-	// Update turn.
-	board.Turn++
-	if board.Turn > 2 {
-		board.Turn = 1
-	}
-	if numOccupiedFields > 1 || board.Move-board.LastRevealed == 4 || revealBoard {
-		// Reveal hidden moves.
-		for r := 0; r < len(board.Fields); r++ {
-			for c := 0; c < len(board.Fields[r]); c++ {
-				f := &board.Fields[r][c]
-				f.Hidden = false
-			}
-		}
-		applyPestEffect(board)
-		// Clean up old special cells.
-		for r := 0; r < len(board.Fields); r++ {
-			for c := 0; c < len(board.Fields[r]); c++ {
-				f := &board.Fields[r][c]
-				if f.occupied() && f.Lifetime == 0 {
-					f.Owner = 0
-					f.Hidden = false
-					f.Type = cellNormal
-					f.Lifetime = cellNormal.lifetime()
-				}
-				if f.Lifetime > 0 {
-					f.Lifetime--
-				}
-			}
-		}
-
-		board.LastRevealed = board.Move
-	}
-	recomputeScoreAndState(board)
-	return true
 }
 
 // Controller function for a running game. To be executed by a dedicated goroutine.
@@ -827,12 +289,12 @@ func (s *Server) gameMaster(game *GameHandle) {
 						delete(playerRmCancel, e.Player.Id)
 					}
 					playerNum = p.playerNum
-				} else if len(players) < allGameTypes[game.gameType].numPlayers {
+				} else if len(players) < gameEngine.NumPlayers() {
 					added = true
 					playerNum = len(players) + 1
 					players[e.Player.Id] = pInfo{playerNum, e.Player}
-					if len(players) == allGameTypes[game.gameType].numPlayers {
-						gameEngine.Init(len(players))
+					if len(players) == gameEngine.NumPlayers() {
+						gameEngine.Start()
 					}
 				}
 				ch := make(chan ServerEvent)
@@ -875,7 +337,11 @@ func (s *Server) gameMaster(game *GameHandle) {
 					// Ignore invalid move request
 					break
 				}
-				if gameEngine.MakeMove(p.playerNum, e) {
+				if s.config.DebugMode {
+					debugReq, _ := json.Marshal(e.MoveRequest)
+					log.Printf("%s: move request: P%d %s", game.Id, p.playerNum, debugReq)
+				}
+				if gameEngine.MakeMove(GameEngineMove{playerNum: p.playerNum, row: e.Row, col: e.Col, cellType: e.Type}) {
 					announcements := []string{}
 					if gameEngine.IsDone() {
 						winner := gameEngine.Winner()
@@ -1076,7 +542,7 @@ func (s *Server) handleMove(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("No game with ID %q", gameId), http.StatusNotFound)
 		return
 	}
-	game.sendEvent(ControlEventMove{PlayerId: player.Id, Row: req.Row, Col: req.Col, Type: req.Type})
+	game.sendEvent(ControlEventMove{PlayerId: player.Id, MoveRequest: req})
 }
 
 func (s *Server) handleReset(w http.ResponseWriter, r *http.Request) {
