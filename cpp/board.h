6e0fbfed34def65cb02627f26f58a56dcc176bd7
Dennis Walter <dennis.walter@gmail.com>
2023-10-29 23:58:08 +0100

    Refactor board into separate file and add tests.

---
+++ cpp/board.h
@@ -0,0 +1,60 @@
+#include <random>
+#include <vector>
+#include <utility>
+
+#include <torch/torch.h>
+
+namespace hexz {
+
+struct Move {
+  int typ;
+  int r;
+  int c;
+  float value;
+};
+
+
+// Torch representation of a hexz board.
+// A board is represented by an (9, 11, 10) tensor. Each 11x10 channel is
+// a one-hot encoding of the presence of specific type of piece/obstacle/etc.
+// The channels are:
+//
+// * 0: flags by P0
+// * 1: cell value 1-5 for P0
+// * 2: cells blocked for P0 (any occupied cell or a cell next to a 5)
+// * 3: next value for P0
+// * 4: flags by P1
+// * 5: cell value 1-5 for P1
+// * 6: cells blocked for P1
+// * 7: next value for P1
+// * 8: grass cells with value 1-5
+//
+// An action is specified by a (2, 11, 10) numpy array. The first 11x10 channel
+// represents a flag move, the second one represents a regular cell move. A
+// flag move must have a single 1 set, a normal move must have a single value
+// 1-5 set.
+class Board {
+ public:
+  static Board RandomBoard();
+
+  // Copy c'tor.
+  Board(const Board& other);
+
+  std::pair<float, float> Score() const;
+  float Result() const;
+
+  int Flags(int player) const;
+
+  void MakeMove(int player, const Move& move); 
+  void OccupyGrass(int player, const Move& move);
+  std::vector<Move> NextMoves(int player) const;
+
+ private:
+  Board();
+
+  static std::mt19937 rng_;
+  torch::Tensor b_;
+  int nflags_[2];
+};
+
+}  // namespace hexz
---
+++ cpp/board_test.cc
@@ -0,0 +1,33 @@
+#include "board.h"
+
+#include <algorithm>
+#include <gtest/gtest.h>
+
+namespace hexz {
+
+std::mt19937 rng{std::random_device{}()};
+
+TEST(BoardTest, PlayFullGame) {
+  Board b = Board::RandomBoard();
+  int player = 0;
+  int n_moves = 0;
+  while (n_moves < 200) {
+    auto moves = b.NextMoves(player);
+    if (moves.empty()) {
+      player = 1 - player;
+      moves = b.NextMoves(player);
+      if (moves.empty()) {
+        break;
+      }
+    }
+    std::shuffle(moves.begin(), moves.end(), rng);
+    b.MakeMove(player, moves[0]);
+    n_moves++;
+  }
+  EXPECT_GT(n_moves, 0);
+  EXPECT_LT(n_moves, 200);
+  EXPECT_EQ(b.Flags(0), 0);
+  EXPECT_EQ(b.Flags(1), 0);
+}
+
+}  // namespace hexz
