a066936deca6c6772628b37627be14d2c85ef708
Dennis Walter <dennis.walter@gmail.com>
2024-11-16 11:37:00 +0100

    Add tests for APM.

--- cpp/perfm.h
+++ cpp/perfm.h
@@ -100,6 +100,7 @@ class Perfm {
 // code blocks and provides averages for these. In other words, APM measures
 // "how much work gets done over time", whereas Perfm measures "how long does a
 // specific code block take to run".
+template <typename Clock = std::chrono::high_resolution_clock>
 class APM {
  public:
   // Reserve twice the capacity needed to retain max_window_seconds.
@@ -108,17 +109,53 @@ class APM {
       : name_(std::move(name)),
         max_window_seconds_(max_window_seconds),
         counts_(max_window_seconds * 2, 0),
-        t_start_(std::chrono::high_resolution_clock::now()) {}
+        t_start_(Clock::now()) {}
 
   // Increment the count of processed work items for the current second by n.
-  void Increment(int n);
+  void Increment(int n) {
+    int64_t d = std::chrono::duration_cast<std::chrono::seconds>(Clock::now() -
+                                                                 t_start_)
+                    .count();
+    std::scoped_lock<std::mutex> lk(mut_);
+    d = AlignCounts(d);
+    counts_[d] += n;
+  }
 
   // Rate returns the rate of items processed per second, averaged over the
   // given window (in seconds). The "current" second (the one for which stats
   // are still being collected), will be added to the window, so that this
   // method returns the rate over a window_seconds +
   // time-passed-in-current-second window.
-  double Rate(int window_seconds);
+  double Rate(int window_seconds) {
+    int64_t us = std::chrono::duration_cast<std::chrono::microseconds>(
+                     Clock::now() - t_start_)
+                     .count();
+    int64_t d = us / 1'000'000;
+    int64_t rem = us % 1'000'000;
+    if (d == 0 && rem == 0) {
+      // Rate was called right after this instance was created.
+      return 0;
+    }
+
+    std::scoped_lock<std::mutex> lk(mut_);
+    d = AlignCounts(d);
+    int64_t w = std::min(static_cast<int64_t>(window_seconds), d);
+    // Accumulate counts_[d - w .. d+1]
+    int64_t sum = std::accumulate(counts_.begin() + (d - w),
+                                  counts_.begin() + (d + 1), 0);
+    double t = static_cast<double>(w) + static_cast<double>(rem) / 1e6;
+    return static_cast<double>(sum) / t;
+  }
+
+  std::vector<int64_t> CountsForTesting() {
+    std::scoped_lock<std::mutex> lk(mut_);
+    return counts_;
+  }
+
+  Clock::time_point t_start() const { 
+    std::scoped_lock<std::mutex> lk(mut_);
+    return t_start_;
+  }
 
  private:
   // Realign counts_ such that d (an offset in seconds from the t_start_)
@@ -126,30 +163,46 @@ class APM {
   // since this method shifts data in counts_ and adjusts t_start_ accordingly.
   //
   // Must only be called while holding a lock on mut_.
-  int64_t AlignCounts(int64_t d);
+  int64_t AlignCounts(int64_t d) {
+    if (d >= counts_.size()) {
+      // Realign s.t. the latest points [d-max_window_seconds..d] are at the
+      // front.
+      size_t offset = d - max_window_seconds_;
+      int i = 0;
+      for (int j = offset; j < counts_.size(); j++) {
+        counts_[i++] = counts_[j];
+      }
+      for (; i < counts_.size(); i++) {
+        counts_[i] = 0;
+      }
+      t_start_ = t_start_ + std::chrono::seconds(offset);
+      return d - offset;
+    }
+    return d;
+  }
 
   const std::string name_;
   const int64_t max_window_seconds_;
   mutable std::mutex mut_;
   std::vector<int64_t> counts_;
-  std::chrono::high_resolution_clock::time_point t_start_;
+  Clock::time_point t_start_;
 };
 
 // Used by workers and MCTS code to count the throughput w.r.t. predictions
 // generated.
-inline APM& APMPredictions() noexcept {
-  static absl::NoDestructor<APM> apm("/predictions", 3600);
+inline APM<>& APMPredictions() noexcept {
+  static absl::NoDestructor<APM<>> apm("/predictions", 3600);
   return *apm;
 }
 // Used by workers and MCTS code to count the throughput w.r.t. examples
 // generated.
-inline APM& APMExamples() noexcept {
-  static absl::NoDestructor<APM> apm("/examples", 3600);
+inline APM<>& APMExamples() noexcept {
+  static absl::NoDestructor<APM<>> apm("/examples", 3600);
   return *apm;
 }
 // Used by workers and MCTS code to count the throughput w.r.t. games generated.
-inline APM& APMGames() noexcept {
-  static absl::NoDestructor<APM> apm("/games", 3600);
+inline APM<>& APMGames() noexcept {
+  static absl::NoDestructor<APM<>> apm("/games", 3600);
   return *apm;
 }
 
---
+++ cpp/perfm_test.cc
@@ -0,0 +1,104 @@
+#include "perfm.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <ratio>
+
+namespace hexz {
+
+namespace {
+
+using ::testing::ElementsAreArray;
+
+// FakeClock is a clock that satisfies std::chrono::is_clock
+// and can be used for testing APM<FakeClock>.
+class FakeClock {
+ public:
+  using rep = int64_t;
+  using period = std::nano;
+  using duration = std::chrono::duration<rep, period>;
+  using time_point = std::chrono::time_point<FakeClock>;
+  static constexpr bool is_steady = false;
+  static time_point now() { return current_time_; }
+  static void advance(const duration& d) { current_time_ += d; }
+
+ private:
+  static inline time_point current_time_{time_point{duration{0}}};
+};
+
+static_assert(std::chrono::is_clock_v<FakeClock>,
+              "FakeClock does not satisfy std::chrono::is_clock!");
+
+TEST(FakeClockTest, Increment) {
+  auto t1 = FakeClock::now();
+  FakeClock::advance(std::chrono::seconds(10));
+  auto t2 = FakeClock::now();
+  EXPECT_EQ(t2 - t1, std::chrono::seconds(10));
+  FakeClock::advance(std::chrono::milliseconds(100));
+  auto t3 = FakeClock::now();
+  EXPECT_EQ(t3 - t1, std::chrono::milliseconds(10100));
+}
+
+TEST(APMTest, IncrementRate) {
+  APM<FakeClock> apm("/test", 5);
+  auto t_start = apm.t_start();
+  FakeClock::advance(std::chrono::seconds(2));
+  apm.Increment(1);
+  apm.Increment(1);
+  double rate = apm.Rate(1);
+  EXPECT_EQ(apm.t_start(), t_start);
+  // Expect 2x as many elements as the window size.
+  std::vector<int64_t> expected_counts = {
+      0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+  };
+  EXPECT_THAT(apm.CountsForTesting(), ElementsAreArray(expected_counts));
+  EXPECT_EQ(apm.Rate(1), 2);
+  EXPECT_EQ(apm.Rate(2), 1);
+  FakeClock::advance(std::chrono::milliseconds(200));
+  EXPECT_FLOAT_EQ(apm.Rate(1), 2 / 1.2);
+}
+
+TEST(APMTest, RateBeforeIncrement) {
+  APM<FakeClock> apm("/test", 5);
+  EXPECT_EQ(apm.Rate(5), 0);
+  FakeClock::advance(std::chrono::milliseconds(200));
+  EXPECT_EQ(apm.Rate(5), 0);
+  FakeClock::advance(std::chrono::milliseconds(1000));
+  EXPECT_EQ(apm.Rate(5), 0);
+}
+
+TEST(APMTest, IncrementExceedWindow) {
+  const int window_size = 5;
+
+  APM<FakeClock> apm("/test", window_size);
+  auto t_start = apm.t_start();
+  FakeClock::advance(std::chrono::milliseconds(10));
+
+  std::vector<int64_t> expected;
+  for (int i = 0; i < window_size * 2; i++) {
+    apm.Increment(i);
+    expected.push_back(i);
+    FakeClock::advance(std::chrono::seconds(1));
+  }
+  EXPECT_THAT(apm.CountsForTesting(), ElementsAreArray(expected));
+
+  // Now the window needs to be realigned:
+  apm.Increment(1);
+  EXPECT_THAT(apm.CountsForTesting(), ElementsAreArray(std::vector<int64_t>{
+                                          5, 6, 7, 8, 9, 1, 0, 0, 0, 0}));
+
+  // Start time should have been adjusted:
+  EXPECT_EQ(apm.t_start() - t_start, std::chrono::seconds(5));
+
+  // Advance one more time, should not realign:
+  FakeClock::advance(std::chrono::seconds(1));
+  apm.Increment(2);
+  EXPECT_THAT(apm.CountsForTesting(), ElementsAreArray(std::vector<int64_t>{
+                                          5, 6, 7, 8, 9, 1, 2, 0, 0, 0}));
+}
+
+}  // namespace
+
+}  // namespace hexz
